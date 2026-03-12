6f2267233ef8daa1f43bd4dbafd0edde60a27a5c
DaHaoTran <nhao4771@gmail.com>
2026-03-12 09:25:10 +0700

    Add new folder

---
+++ src/app/api/bikes/by_types/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import path from 'path';
+import fs from 'fs';
+
+const filePath = path.join(process.cwd(), "public", "jsons", "bikes.json");
+const getBikes = () => JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+export async function GET(request) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const typeId = searchParams.get("id");
+        const bikes = getBikes().filter(x => x.typeId === parseInt(typeId));
+        if(!bikes) return NextResponse.json({message: "Not Found !"}, {status: 404});
+        return NextResponse.json(bikes);
+    } catch (error) {
+        return NextResponse.json({message: error.message}, {status: 500});
+    }
+}
