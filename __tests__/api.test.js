72d1b46065390600fc004a89e0362d8a6ff366d0
DaHaoTran <nhao4771@gmail.com>
2026-03-12 18:58:22 +0700

    Add new test case

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -10,6 +10,10 @@ describe('bikes api', () => {
       const res = await fetch('api/bikes/by_types?id=1');
       expect(res.ok).toBe(true);
     })
+    it('get bikes by id wih limit', async () => {
+      const res = await fetch('api/bikes/by_types?id=1&limit=1');
+      expect(res.ok).toBe(true);
+    })
 })
 
 describe('types api', () => {
