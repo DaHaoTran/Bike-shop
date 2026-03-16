3513d14f1073f6dab5892c150eeb21125897c9e0
DaHaoTran <nhao4771@gmail.com>
2026-03-16 22:24:35 +0700

    Fix generate pages

--- src/app/pages/errors/[number]/page.js
+++ src/app/pages/errors/[number]/page.js
@@ -1,26 +1,9 @@
-"use client"
-import React from 'react'
-import styles from './page.module.css'
-import { useParams, useSearchParams } from 'next/navigation'
-
-let params;
-
-export function generateStaticParams() {
-  const params = useParams()
-
-  return params;
+export async function generateStaticParams() {
+  return [{ number: '404' }, { number: '500' }]
 }
 
-export default function Error({ params }) {
-  return (
-    <div className={styles.error_container}>
-      <div>
-        <h1 className={styles.error_number_label}>{params.number}</h1>
-        <div className={styles.error_button_container}>
-          <button className='px-4' onClick={(x => document.location.href= '/')}>Reload</button>
-        </div>
-      </div>
-      <h5 className={styles.error_sub_title}>Something <br /> went wrong :(( <br /> ...</h5>
-    </div>
-  )
+import ErrorClient from './ErrorClient'
+
+export default function Page({ params }) {
+  return <ErrorClient number={params.number} />
 }
