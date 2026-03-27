82de1632024fede6ab179bd24b8688a2e98f8cd3
DaHaoTran <nhao4771@gmail.com>
2026-03-27 22:05:15 +0700

    Update code

--- src/app/methods/list.js
+++ src/app/methods/list.js
@@ -2,34 +2,32 @@
 // import path from 'path';
 // import fs from "fs";
 
+import { useFetch } from "../hooks/useFetch";
+
 // const filePathDefault = path.join(process.cwd(), "src", "assets", "jsons");
 
 const getTypeList = async () => {
     // const filePath = path.join(filePathDefault, "types.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const res = await fetch('https://dahaotran.github.io/Bike-shop/jsons/types.json');
-    return res.json();
+    return useFetch('https://dahaotran.github.io/Bike-shop/jsons/types.json');
 }
 
 const getBikeList = async () => {
     // const filePath = path.join(filePathDefault, "bikes.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const res = await fetch('https://dahaotran.github.io/Bike-shop/jsons/bikes.json');
-    return res.json();
+    return useFetch('https://dahaotran.github.io/Bike-shop/jsons/bikes.json');
 }
 
 const getFirmList = async () => {
     // const filePath = path.join(filePathDefault, "firms.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const res = await fetch('https://dahaotran.github.io/Bike-shop/jsons/firms.json');
-    return res.json();
+    return useFetch('https://dahaotran.github.io/Bike-shop/jsons/firms.json');
 }
 
 const getDetailList = async () => {
     // const filePath = path.join(filePathDefault, "bike_details.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const res = await fetch('https://dahaotran.github.io/Bike-shop/jsons/bike_details.json');
-    return res.json();
+    return useFetch('https://dahaotran.github.io/Bike-shop/jsons/bike_details.json');
 }
 
 export { getTypeList, getBikeList, getFirmList, getDetailList };
